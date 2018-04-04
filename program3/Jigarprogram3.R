35ce5a1f7252eef2d92b295e4f3d35e1d28232bb
JigarDP <developerjdp@gmail.com>
2018-04-03 23:22:41 -0400

    updated program3

--- program3/Jigarprogram3.R
+++ program3/Jigarprogram3.R
@@ -1,5 +1,4 @@
-.Commands <- setRefClass("Commands", methods = list(
-  
+Commands <- setRefClass("Commands", methods = list(
   myList <<- c(),
   myString <<- "",
   number1 <<- 0,
@@ -37,14 +36,45 @@
     }
   }
   
-  
 ))
 
-add("Hey")
-add("f")
-add("j")
-show()
-move(3,1)
-show()
-complete(2)
-show()
+main <- function() {
+  cat("\n* * * To Do List * * *\n")
+  response <- readline(prompt = "Enter a command(Show, Add, Move, Complete) or End: ")
+  response <- strsplit(response, " ")
+  x <- toString(response[[1]][1])
+  
+  while (a != "End") {
+    tryCatch ({
+      if (a == "Show") {
+        listCopy = show()
+        for (item in 1:length(listCopy)) {
+          cat(sprintf("%i = %0.5s\n", item, myList2[[item]]))
+        }
+      }
+      else if (a == "Add") {
+        temporaryString = response.mkString(" ")
+        responseTemp = temporaryString.split(" ", 2)
+        string = responseTemp(1)
+        myToDoList.add(string)
+      }
+      else if (a == "Move") {
+        num1 = response(1).toInt
+        num2 = response(2).toInt
+        myToDoList.move(num1, num2)
+      }
+      else if (a == "Complete") {
+        num = response(1).toInt
+        myToDoList.complete(num)
+      }
+      else {
+        println(a + " is an unrecognized command")
+      }
+    })
+  }
+  
+  
+}
+
+main()
+
